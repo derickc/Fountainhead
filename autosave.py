bef64dc343af824f331676f8992482eb841530c7
Derick <derickc@gmail.com>
2014-11-27 17:34:13 -0800

    Add Sublime Text 2 support. Move color schemes to dedicated folder.

--- autosave.py
+++ autosave.py
@@ -6,11 +6,19 @@ class AutomaticSaveCommand(sublime_plugin.EventListener):
 
     clicks = 0
 
-    def on_modified(self, view):
-        if view.settings().get('syntax') == 'Packages/Fountainhead/Fountainhead.tmLanguage':
+    def save_me(self, view):
+        if view.settings().get('syntax') == 'Packages/Fountainhead/Fountainhead.tmLanguage' and int(sublime.version()) < 3000:
             if sublime.load_settings('Fountainhead.sublime-settings').get('auto_save', True):
                 self.clicksTrigger = sublime.load_settings('Fountainhead.sublime-settings').get('auto_save_count', 42)
                 self.clicks += 1
                 if self.clicks >= self.clicksTrigger:
                     self.clicks = 0
                     view.run_command('save')
+
+    def on_modified(self, view):
+        if int(sublime.version()) < 3000:
+            self.save_me(view)
+
+    def on_modified_async(self, view):
+        if int(sublime.version()) >= 3000:
+            self.save_me(view)
--- hide.py
+++ hide.py
@@ -31,16 +31,9 @@ class HideBoneyardCommand(sublime_plugin.TextCommand):
         def run(self, edit):
             if self.view.fold(self.view.find_by_selector('comment')):
                 self.view.fold(self.view.find_by_selector('comment'))
-                print('hide')
             else:
-                ShowBoneyardCommand.run(self, edit)
-
-
-class ShowBoneyardCommand(sublime_plugin.TextCommand):
-
-        def run(self, edit):
-            for region in self.view.find_by_selector('comment'):
-                self.view.unfold(region)
+                for region in self.view.find_by_selector('comment'):
+                    self.view.unfold(region)
 
 
 class HideSynopsesCommand(sublime_plugin.TextCommand):
@@ -49,14 +42,8 @@ class HideSynopsesCommand(sublime_plugin.TextCommand):
             if self.view.fold(self.view.find_by_selector('meta.diff')):
                 self.view.fold(self.view.find_by_selector('meta.diff'))
             else:
-                ShowSynopsesCommand.run(self, edit)
-
-
-class ShowSynopsesCommand(sublime_plugin.TextCommand):
-
-        def run(self, edit):
-            for region in self.view.find_by_selector('meta.diff'):
-                self.view.unfold(region)
+                for region in self.view.find_by_selector('meta.diff'):
+                    self.view.unfold(region)
 
 
 class HideNotesCommand(sublime_plugin.TextCommand):
@@ -65,13 +52,7 @@ class HideNotesCommand(sublime_plugin.TextCommand):
             if self.view.fold(self.view.find_by_selector('variable.parameter')):
                 self.view.fold(self.view.find_by_selector('variable.parameter'))
             else:
-                ShowNotesCommand.run(self, edit)
-
-
-class ShowNotesCommand(sublime_plugin.TextCommand):
-
-        def run(self, edit):
-            for region in self.view.find_by_selector('variable.parameter'):
-                self.view.unfold(region)
+                for region in self.view.find_by_selector('variable.parameter'):
+                    self.view.unfold(region)
 
 # http://www.sublimetext.com/forum/viewtopic.php?f=3&t=4620
